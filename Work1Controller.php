a4cf4206540ff1c9b394677e5f8fecb46c0d7a9e
AYA <60229621+AYA-kurokiri09@users.noreply.github.com>
2020-06-02 01:16:27 +0900

    Update Work1Controller.php
    Implement validation

--- Work1Controller.php
+++ Work1Controller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Request;
 use Illuminate\Http\Response;
+use App\Http\Requests\Work1Request;
 
 
 class Work1Controller extends Controller
@@ -16,8 +17,9 @@ class Work1Controller extends Controller
         return view('work1.create');
     }
 
-    public static function home (Request $createdData) {
-        $name = $createdData::all();
+    public static function home (Work1Request $request) {
+        $validated = $request->validated();
+        $name = $request->all();
         return view('work1.home', compact('name'));
     }
 }
